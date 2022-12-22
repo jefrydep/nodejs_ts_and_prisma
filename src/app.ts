4b889f2f5edc51dac5442f0ef3496083d44cd828
jefry <jefrydep@gmail.com>
2022-12-22 10:48:59 -0500

    creating files mvm

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+
 import morgan from "morgan";
 import { PrismaClient } from "@prisma/client";
-
+//importamos la ruta de estamanera
+// import { router } from "./routes/item";
 const app = express();
 require("dotenv").config();
 
@@ -11,7 +13,8 @@ const PORT = process.env.PORT;
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-
+// app.use(router);
+// 
 const prisma = new PrismaClient();
 
 app.get("/", (req: Request, res: Response) => {
