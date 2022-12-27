cd62779276fd42c4f90041912a7c1b2c13b3a6cb
jefry <jefrydep@gmail.com>
2022-12-27 10:57:31 -0500

    fixed all routes

---
+++ prisma/migrations/20221227153307_noddt/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `department` on the `Patient` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Patient" DROP COLUMN "department",
+ADD COLUMN     "departament" TEXT;
