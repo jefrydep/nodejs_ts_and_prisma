1e777c706940efcaa70fd67cd3080c1e88ebfad8
jefry <jefrydep@gmail.com>
2022-12-30 18:35:28 -0500

    doing pull from merge to another branch

--- prisma/migrations/20221227153307_noddt/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `department` on the `Patient` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Patient" DROP COLUMN "department",
-ADD COLUMN     "departament" TEXT;
